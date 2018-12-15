6ef6cc7d9d9086692e0b33605dff957f971c7979
Caio Andrade <caioertai@gmail.com>
2018-12-15 21:12:16 -0200

    Initializing circles on target

--- src/circle.js
+++ src/circle.js
@@ -3,7 +3,47 @@ import Circle from './circle'
 class Circle {
   constructor(circleElement) {
     this.elem = circleElement
-    console.log(this.elem);
+  }
+
+  has_location(location) {
+    const center = this.center()
+    const xP = location.x;
+    const yP = location.y;
+    const xC = center.x
+    const yC = center.y
+
+    const distanceFromCenter = Math.sqrt(
+      Math.pow((xP - xC), 2) + Math.pow((yP - yC), 2)
+    )
+
+    return distanceFromCenter < this.radius();
+  }
+
+  center() {
+    return {
+      x: (this.width() / 2) + this.leftEdge(),
+      y: (this.height() / 2) + this.topEdge()
+    }
+  }
+
+  radius() {
+    return this.width() / 2;
+  }
+
+  width() {
+    return this.elem.offsetWidth
+  }
+
+  height() {
+    return this.elem.offsetHeight
+  }
+
+  leftEdge() {
+    return this.elem.offsetLeft
+  }
+
+  topEdge() {
+    return this.elem.offsetTop
   }
 }
 
--- src/target.js
+++ src/target.js
@@ -1,50 +1,15 @@
 import Circle from './circle'
 
 class Target {
-  constructor(circleSelector) {
-    this.elem = document.querySelector(circleSelector);
-    console.log(this.center());
+  constructor(targetSelector) {
+    this.elem = document.querySelector(targetSelector);
+    this.circles = this._initializeCircles();
   }
 
-  has_location(location) {
-    const center = this.center()
-    const xP = location.x;
-    const yP = location.y;
-    const xC = center.x
-    const yC = center.y
-
-    const distanceFromCenter = Math.sqrt(
-      Math.pow((xP - xC), 2) + Math.pow((yP - yC), 2)
-    )
-
-    return distanceFromCenter < this.radius();
-  }
-
-  center() {
-    return {
-      x: (this.width() / 2) + this.leftEdge(),
-      y: (this.height() / 2) + this.topEdge()
-    }
-  }
-
-  radius() {
-    return this.width() / 2;
-  }
-
-  width() {
-    return this.elem.offsetWidth
-  }
-
-  height() {
-    return this.elem.offsetHeight
-  }
-
-  leftEdge() {
-    return this.elem.offsetLeft
-  }
-
-  topEdge() {
-    return this.elem.offsetTop
+  _initializeCircles() {
+    return [...this.elem.children].map(circle => {
+      return new Circle(circle)
+    })
   }
 }
 
